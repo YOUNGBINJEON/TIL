a4e84ae2a1e852660e42ce2325c4808b54428d0d
YOUNGBINJEON <sh44972@gmail.com>
2021-04-20 23:38:32 +0900

    [Edit]LinkedList.java

--- workspaceForJava/DataStructure/LinkedList/main.java
+++ workspaceForJava/DataStructure/LinkedList/main.java
@@ -4,7 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		LinkedList numbers = new LinkedList();
-		
+		numbers.addFirst(30);
+		numbers.addFirst(20);
+		numbers.addFirst(10);
+		numbers.addLast(40);
+		numbers.addLast(50);
 		
 		
 
