769c5cb994a3effdb7dbde608561b4ef721defd3
YOUNGBINJEON <sh44972@gmail.com>
2021-08-31 22:01:43 +0900

    [Add] Servlet, JSP, MVC패턴 - MVC프레임워크 만들기 - 유연한 컨트롤러1 V5

---
+++ Spring MVC 1/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,12 @@
+package hello.servlet.web.frontcontroller.v5;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+}
