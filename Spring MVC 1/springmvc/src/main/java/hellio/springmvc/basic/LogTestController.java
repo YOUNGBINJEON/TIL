59a0bb204b45abd1d15a58da09743989cd2dded2
YOUNGBINJEON <sh44972@gmail.com>
2021-09-18 22:15:46 +0900

    [Add] Servlet, JSP, MVC패턴 - 스프링 MVC 기본기능 - 로깅 간단히 알아보기

--- Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/LogTestController.java
+++ Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/LogTestController.java
@@ -1,13 +1,17 @@
 package hellio.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    // @Slf4j가 아래 코드 자동 등록
+    //private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
