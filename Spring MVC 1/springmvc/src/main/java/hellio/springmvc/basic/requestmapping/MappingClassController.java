4d83e04a7522af6a7adb8dec9b1c2356872e7587
YOUNGBINJEON <sh44972@gmail.com>
2021-09-22 22:58:48 +0900

    [Add] Servlet, JSP, MVC패턴 - 스프링 MVC 기본기능 - 요청 매핑 - API예시

--- Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/requestmapping/MappingClassController.java
+++ Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/requestmapping/MappingClassController.java
@@ -4,29 +4,30 @@ package hellio.springmvc.basic.requestmapping;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/mapping/users")
 public class MappingClassController {
 
-    @GetMapping("/mapping/users")
+    @GetMapping
     public String user() {
         return "get users";
     }
 
-    @PostMapping("/mapping/users")
+    @PostMapping
     public String addUsers() {
         return "post user";
     }
 
-    @GetMapping("/mapping/users/{userId}")
+    @GetMapping("/{userId}")
     public String findUser(@PathVariable String userId) {
         return "update userId= " + userId;
     }
 
-    @PatchMapping("/mapping/users/{userId}")
+    @PatchMapping("/{userId}")
     public String updateUser(@PathVariable String userId) {
         return "update userId=" + userId;
     }
 
-    @DeleteMapping("/mapping/users/{userId}")
+    @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable String userId) {
         return "delete userId=" + userId;
     }
