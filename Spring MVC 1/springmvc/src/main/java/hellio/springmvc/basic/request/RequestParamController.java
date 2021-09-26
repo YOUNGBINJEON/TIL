05ef36eaabb6d90a08f0d077c2c43510d6236104
YOUNGBINJEON <sh44972@gmail.com>
2021-09-26 17:16:00 +0900

    [Add] Servlet, JSP, MVC패턴 - 스프링 MVC 기본기능 - HTTP 요청 - @RequestParam

--- Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/request/RequestParamController.java
+++ Spring MVC 1/springmvc/src/main/java/hellio/springmvc/basic/request/RequestParamController.java
@@ -3,6 +3,8 @@ package hellio.springmvc.basic.request;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,5 +21,37 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    // @ResponseBody를 사용하면 "ok"를 Http 응답 메시지에 넣어 바로 반환 @RestController과 같은 효과
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                                 @RequestParam("age") int memberAge) {
+
+        log.info("username={}, age={}",  memberName, memberAge);
+        return "ok";
+
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age) {
+
+        log.info("username={}, age={}",  username, age);
+        return "ok";
+
+    }
+
+    //애노테이션을 너무 줄이는 것은 명확하게 확인하기 어려운 단점도 존재
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age) {
+
+        log.info("username={}, age={}",  username, age);
+        return "ok";
+
     }
 }
