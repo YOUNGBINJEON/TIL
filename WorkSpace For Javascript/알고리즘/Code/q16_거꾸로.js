a627093ad46cc38bc8b6879339d3cdf0b3f4066a
YOUNGBINJEON <sh44972@gmail.com>
2021-11-27 22:58:54 +0900

    [Add] 자바스크립트 100제 - ~q17

---
+++ WorkSpace For Javascript/알고리즘/Code/q16_거꾸로.js
@@ -0,0 +1,8 @@
+function reverse(s) {
+  return s.split('').reverse().join("");
+}
+
+let str = reverse("거꾸로 문자를 출력합니다.");
+
+
+console.log(str);
---
+++ WorkSpace For Javascript/알고리즘/Code/q17_놀이기구 키 제한.js
@@ -0,0 +1,9 @@
+
+let num = prompt("키를 입력해주세요.");
+
+if(num > 150) {
+  console.log("YES");
+}
+else{
+  console.log("NO");
+}
