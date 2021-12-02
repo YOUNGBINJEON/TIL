00e28878501b0037bac97d0e5bf88b49cf9464b8
YOUNGBINJEON <sh44972@gmail.com>
2021-12-02 15:59:24 +0900

    [Add] 자바스크립트 100제 - ~q24

---
+++ WorkSpace For Javascript/알고리즘/Code/q21_set.js
@@ -0,0 +1,7 @@
+const list = [1, 2, 2, 2, 3, 4, 5, 5, 5, 6, 7];
+
+// 중복되지 않은 데이터를 저장하는 데이터 구조이다.
+
+let setVal = new Set(list);
+
+console.log(setVal);
---
+++ WorkSpace For Javascript/알고리즘/Code/q24_대문자로 변환.js
@@ -0,0 +1,3 @@
+let sentence = prompt("이름을 입력해주세요.").replace();
+
+console.log(sentence.toUpperCase());
