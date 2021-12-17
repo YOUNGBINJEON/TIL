ec3f07fa2659b5b10120770dbd598591d984745b
YOUNGBINJEON <sh44972@gmail.com>
2021-12-17 18:45:58 +0900

    [Add] 자바스크립트  100제 - ~q37

---
+++ WorkSpace For Javascript/알고리즘/Code/q37_반장선거.js
@@ -0,0 +1,17 @@
+const array = ["영빈", "불휘", "한울", "한울", "영빈", "불휘", "영빈", "영빈", "한울", "영빈", "영빈"]
+let result = {};
+let winner = "";
+
+for(let index in array) {
+  let val = array[index];
+  result[val] = result[val] === undefined ? 1 : result[val] + 1;
+}
+
+winner = Object.keys(result).reduce(function(a, b){
+  console.log(a, b);
+  eturn result[a] > result[b] ? a : b
+
+});
+
+console.log(winner);
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
