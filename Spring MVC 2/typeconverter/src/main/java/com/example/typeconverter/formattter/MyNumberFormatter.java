28682c60d79cea52f4de1e23808e4d724aa3a55f
YOUNGBINJEON <sh44972@gmail.com>
2022-05-18 23:27:07 +0900

    [Add] 스프링 핵심원리 고급편 - 예제 만들기 - 예제 프로젝트 만들기 - V0

--- Spring MVC 2/typeconverter/src/main/java/com/example/typeconverter/formattter/MyNumberFormatter.java
+++ Spring MVC 2/typeconverter/src/main/java/com/example/typeconverter/formattter/MyNumberFormatter.java
@@ -13,7 +13,8 @@ public class MyNumberFormatter implements Formatter<Number> {
 
     @Override
     public Number parse(String text, Locale locale) throws ParseException {
-        log.info(("text={}, locale={}", text, locale));
+        log.info("text={}, locale={}", text, locale);
+        //"1,000" -> 1000
         NumberFormat format = NumberFormat.getInstance(locale);
         return format.parse(text);
     }
@@ -21,6 +22,7 @@ public class MyNumberFormatter implements Formatter<Number> {
     @Override
     public String print(Number object, Locale locale) {
         log.info("object={}, locale={}", object, locale);
+        //1000 -> "1,000"
         return NumberFormat.getInstance(locale).format(object);
     }
 
